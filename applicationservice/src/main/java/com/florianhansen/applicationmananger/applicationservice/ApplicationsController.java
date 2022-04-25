b51a9f12c8a8358d5b64f3aab168994b4de9daaf
Florian Hansen <flhansen.93@gmail.com>
2022-04-26 01:48:49 +0200

    Add unit tests for application service.

--- applicationservice/src/main/java/com/florianhansen/applicationmananger/applicationservice/ApplicationsController.java
+++ applicationservice/src/main/java/com/florianhansen/applicationmananger/applicationservice/ApplicationsController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 
 import com.florianhansen.applicationmanager.jwt.util.JwtUtil;
 import com.florianhansen.applicationmanager.model.Application;
---
+++ applicationservice/src/test/java/com/florianhansen/applicationmanager/applicationservice/TypesTest.java
@@ -0,0 +1,73 @@
+package com.florianhansen.applicationmanager.applicationservice;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import com.florianhansen.applicationmanager.jwt.util.JwtUtil;
+import com.florianhansen.applicationmananger.applicationservice.ApplicationServiceApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationServiceApplication.class)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@TestInstance(Lifecycle.PER_CLASS)
+@AutoConfigureMockMvc
+public class TypesTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	private String token;
+	
+	@BeforeAll
+	private void beforeAll() { 
+		token = jwtUtil.generateToken("testuser", 0);
+	}
+	
+	@Test
+	public void getWorkTypesUnauthorized() throws Exception {
+		mockMvc
+			.perform(get("/api/types/worktypes"))
+			.andExpect(status().is(403));
+	}
+	
+	@Test
+	public void getWorkTypesAuthorized() throws Exception {
+		mockMvc
+			.perform(get("/api/types/worktypes")
+				.header("Authorization", "Bearer " + token))
+			.andExpect(status().is(200));
+	}
+	
+	@Test
+	public void getStatusesUnauthorized() throws Exception {
+		mockMvc
+			.perform(get("/api/types/statuses"))
+			.andExpect(status().is(403));
+	}
+	
+	@Test
+	public void getStatusesAuthorized() throws Exception {
+		mockMvc
+			.perform(get("/api/types/statuses")
+				.header("Authorization", "Bearer " + token))
+			.andExpect(status().is(200));
+
+	}
+
+}
